81cf0faf96383bad26b2d2fbdeefa17d44cf8c1d
Bidek56 <darek.bidek@gmail.com>
2024-08-31 13:44:37 -0400

    Updating packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = getByText("User name");
+  expect(linkElement).toBeDefined();
 });
